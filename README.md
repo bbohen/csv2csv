c3472017d0ed4e5a26e0d588c29ba0319d3c3079
Brad Bohen <bbohen@gmail.com>
2017-12-10 19:46:53 -0500

    Updated README and did some cleanup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # csv2csv
+
+This is a small script that takes a .csv file and transforms it into a _different_ format based on some mappings.
+
+## Instructions
+
+1. Install Python 3 if needed.
+2. Place your .csv file you wish to transform within the directory of this repo and name it `input.csv`.
+3. Create another .csv file with the name of `new_headers.csv` in the following format.
+   1. The first row are the headers desired for the transformed format.
+   2. The second row are the corresponding names of the headers from the input .csv. If this is a new column with no data, just leave the value blank.
+   3. Here is an example of [input.csv](input_example.csv) & [new_headers.csv](new_headers_example.csv)
+4. `python main.py`
+5. `open output.csv`
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from collections import OrderedDict
 import csv
 
-with open('new_headers.csv', 'r') as csvfile:
+INPUT_FILE = 'input.csv'
+NEW_HEADER_FILE = 'new_headers.csv'
+OUTPUT_FILE = 'output.csv'
+
+with open(NEW_HEADER_FILE, 'r') as csvfile:
     new_headers_csv = csv.reader(csvfile)
     fieldnames = next(new_headers_csv)
     fieldname_mappings = next(new_headers_csv)
 
-with open('input.csv', 'r') as infile, open('output.csv', 'a') as outfile:
+with open(INPUT_FILE, 'r') as infile, open(OUTPUT_FILE, 'a') as outfile:
     writer = csv.DictWriter(
         outfile, extrasaction='ignore', fieldnames=fieldnames)
     writer.writeheader()
@@ -33,12 +37,12 @@ with open('input.csv', 'r') as infile, open('output.csv', 'a') as outfile:
     def mapper(item):
         value = item[1]
         try:
-            indices = [i for i, x in enumerate(
+            matching_indices = [i for i, x in enumerate(
                 fieldname_mappings) if x == item[0]]
-            field_name_index = fieldname_mappings.index(item[0])
-            if len(indices) > 1:
+            if len(matching_indices) > 1:
                 key = item[0]
             else:
+                field_name_index = fieldname_mappings.index(item[0])
                 key = fieldnames[field_name_index]
         except (IndexError, ValueError):
             key = item[0]
@@ -46,8 +50,10 @@ with open('input.csv', 'r') as infile, open('output.csv', 'a') as outfile:
         return (key, value)
 
     for row in csv.DictReader(infile):
-        row_items = list(row.items())
-        updated_items = adjust_row(row_items)
-        mapped_items = list(map(mapper, updated_items))
+        row_list = list(row.items())
+        adjusted_row_list = adjust_row(row_list)
+        mapped_items = list(map(mapper, adjusted_row_list))
 
         writer.writerow(OrderedDict(mapped_items))
+
+    print('{} has been created'.format(OUTPUT_FILE))
